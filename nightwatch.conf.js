1214acc0ca60d9ea36ca09b34d58aae101c2177c
ramonjoaquim <limasramon3@gmail.com>
2019-08-17 10:41:10 -0300

    Create nightwatch.conf.js

---
+++ nightwatch.conf.js
@@ -0,0 +1,25 @@
+const chromedriver = require('chromedriver');
+
+module.exports = {
+  test_settings: {
+    default: {
+      webdriver: {
+        start_process: true,
+        server_path: 'node_modules/chromedriver/lib/chromedriver/chromedriver.exe',
+        port: 9515
+      },
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+            args: ['incognito', 'no-sandbox', 'disable-gpu']
+        }
+      },
+      screenshots:{
+          enabled:true,
+          path:'report/screenshot'
+      }
+    }
+  }
+};
